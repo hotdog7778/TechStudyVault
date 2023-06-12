3fa5af3ca11bb6468b3131ae02113f260dc7f725
김태균 <rlaxorbs159@gmail.com>
2023-06-12 16:22:18 +0900

    팬명록 프로젝트 온도 api 사용

--- project/05.fan/app.py
+++ project/05.fan/app.py
@@ -16,4 +16,4 @@ def guestbook_get():
     return jsonify({'msg': 'GET 연결 완료!'})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0', port=5001, debug=True)
