e7512dff3b15a4f3280a28c50ca6ea500ccf5072
김태균 <rlaxorbs159@gmail.com>
2023-06-12 16:46:22 +0900

    팬명록 프로젝트GET

--- project/05.fan/app.py
+++ project/05.fan/app.py
@@ -26,7 +26,8 @@ def guestbook_post():
 
 @app.route("/guestbook", methods=["GET"])
 def guestbook_get():
-    return jsonify({'msg': 'GET 연결 완료!'})
+    all_comments = list(db.fan.find({},{'_id':False}))
+    return jsonify({'result': all_comments})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5001, debug=True)
