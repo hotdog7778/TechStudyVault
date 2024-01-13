3ca048adf60781711c2ce772c9a17660b85fd04e
hotdog7778 <rlaxorbs159@gmail.com>
2024-01-13 23:37:37 +0900

    const,let 변수선언 ts 컴파일 에러

--- TS-study/practice/src/index.ts
+++ TS-study/practice/src/index.ts
@@ -1,3 +1,16 @@
 const msg: string = 'Hello, world';
-
+// const msg: number = 'Hello, world'; // 타입에러 발생
 console.log(msg);
+
+const greeting: string = 'Hello, ';
+const target: string = 'world!';
+console.log(greeting + target);
+
+let first: string, second: string;
+first = 'hello, ';
+second = 'world@';
+console.log(first + second);
+
+let first2: string, second2: string;
+first2 = 'hello, ';
+// console.log(first2 + second2); // 타입 선언 후 값을 할당하지 않고 사용하여 컴파일 에러 발생
