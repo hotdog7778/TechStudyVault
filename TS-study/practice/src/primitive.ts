006319f2a71944ef1bc6882de63e9b6add77e869
hotdog7778 <rlaxorbs159@gmail.com>
2024-01-15 23:29:55 +0900

    typescript: string, boolean type

--- TS-study/practice/src/primitive.ts
+++ TS-study/practice/src/primitive.ts
@@ -39,4 +39,28 @@ console.log(million); // 1000000
 const bignum: bigint = (123n + 456n) * 2n;
 console.log(bignum); // 1158n
 const result = 5n / 2n;
-console.log(result);
+console.log(result); // 2n // BigInt는 정수밖에 처리하지 못하고 결과가 실수가 된다면 소수점 자리는 버려진다.
+
+// number는 number 끼리, bigint는 bigint 끼리 계산해야한다
+// const wrong = 100n + 50; // 컴파일 에러 발생
+
+// 문자열 리터럴
+// const message: string = 'Hello \n world';
+// console.log(message);
+
+// 템플릿 리터럴
+const message: string = `Hello 
+world!`;
+console.log(message);
+
+const str1: string = 'Hello';
+const str2: string = 'world';
+console.log(`${str1}, ${str2}`); // Hello, world
+
+// 이스케이프 시퀀스
+console.log('Hello \u{1f600} world!');
+
+// boolean
+const no: boolean = false;
+const yes: boolean = true;
+console.log(yes, no);
